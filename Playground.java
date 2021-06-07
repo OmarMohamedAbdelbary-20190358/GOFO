056fd3e0b32680c1a270d8cce1e144cf3194158f
OmarMohamedAbdelbary-20190358 <84973108+OmarMohamedAbdelbary-20190358@users.noreply.github.com>
2021-06-07 02:14:23 +0200

    Update Playground.java

--- Playground.java
+++ Playground.java
@@ -1,5 +1,24 @@
 package GoFo;
 
-public class Playground { i love;
-                         
+
+public class Playground {
+private String PGname;
+private String PGlocation;
+private String Pgsize;
+private String PGavailbleHours;
+private float cancelperiod;
+private float PGprice;
+private boolean Active;
+
+public void Playground(String PGname,String PGlocation,String PGsize,float PGavialbleHours,float PGprice,float PGcancelperiod) {
+	this.PGname=PGname;
+	this.PGlocation=PGlocation;
+	this.PGprice=PGprice;
+	this.PGavailbleHours=PGavailbleHours;
+	this.cancelperiod=cancelperiod;
+	this.Active=Active;
+	this.Pgsize=PGsize;
 }
+
+}
+
