51c4ec68f81b89775f119873b49ec7ff1e0a6715
Kariimayman <Kareemayman8@gmail.com>
2021-06-08 01:55:12 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,8 @@
 package GoFo;
 import java.util.Scanner;
 
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public static void main(String[] args) {
+		user P1;
+		P1.login();
 
 	}
-
-}
